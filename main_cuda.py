1f7f337d696ed7f14e9e1212aa47526623357ee9
Yunanana <nannun1510@hotmail.com>
2021-08-30 15:48:26 +0700

    done

--- main_cuda.py
+++ main_cuda.py
@@ -20,47 +20,30 @@ net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 model = cv2.dnn_DetectionModel(net)
 model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
 
-def readvid(i):
-    vod = cv2.VideoCapture("./img/demo.mp4")
-    while True:
-        try:
-            ret, frame = vod.read()
-            gpu_frame = cv2.cuda_GpuMat()
-            gpu_frame.upload(frame)
-            img = cv2.cuda.cvtColor(gpu_frame, cv2.COLOR_BGR2BGRA)
-            img = cv2.cuda.resize(img, (int(1280 * scale), int(720 * scale)))
-            frames[i] = img
-        except:
-            pass
+vod = cv2.VideoCapture("./img/demo.mp4")
+while True:
+        ret, frame = vod.read()
+        gpu_frame = cv2.cuda_GpuMat()
+        gpu_frame.upload(frame)
+        #img = cv2.cuda.cvtColor(gpu_frame, cv2.COLOR_BGR2BGRA)
+        #img = cv2.cuda.resize(img, (int(1280 * scale), int(720 * scale)))
+        frame = gpu_frame.download()
 
-def showvid():
-    while True:
-        try:
-            start = time()
-            frame = frames[1]
-            classes, scores, boxes = model.detect(frame, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
-            frame = frame.download()
-            for (classid, score, box) in zip(classes, scores, boxes):
-                color = COLORS[int(classid) % len(COLORS)]
-                label = "%s : %f" % (class_names[classid[0]], score)
-                cv2.rectangle(frame, box, color, 2)
-                cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-            end = time()
+        start = time()
+        classes, scores, boxes = model.detect(frame, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+        for (classid, score, box) in zip(classes, scores, boxes):
+            color = COLORS[int(classid) % len(COLORS)]
+            label = "%s : %f" % (class_names[classid[0]], score)
+            cv2.rectangle(frame, box, color, 2)
+            cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        end = time()
 
-            fps_label = "FPS: %.2f " % (1 / (end - start))
-            cv2.putText(frame, fps_label, (0, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            cv2.imshow('ss', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        except:
-            pass
+        fps_label = "FPS: %.2f " % (1 / (end - start))
+        cv2.putText(frame, fps_label, (0, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.imshow('ss', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
-if __name__ == '__main__':
-    load = threading.Thread(target=readvid,args=(1,))
-    show = threading.Thread(target=showvid,args=())
-
-    load.start()
-    show.start()
 
 
